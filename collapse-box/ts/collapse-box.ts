5969388d3b7a819bdbf6fdc6b13ba70b793581a2
A.D <ad@Alexandrs-MacBook-Pro.local>
2023-01-17 15:38:42 -0700

    increment

--- collapse-box/ts/collapse-box.ts
+++ collapse-box/ts/collapse-box.ts
@@ -1,52 +1,84 @@
+interface ICollapseBox{
+    adId: string;
+    componentTitle: string;
+
+    close():void;
+    open():void;
+    highlightTitle():void;
+    unHighlightTitle():void;
+    
+    render(values:object):string;
+    addButton(html:string, action: (event:Event)=> void):void;
+}
+
+interface IContent {
+    content:string;
+}
+
 /**
  * Custom component for ad-collapse-box html tag
  * window.customElements.define('ad-collapse-box', ADCollapseBox);
  * Usage:
  *  ad-title
  *  ad-id
- *  ad-initial-state
+ *  ad-initial-state 
  */
-class ADCollapseBox extends HTMLElement {
-    get adId() {
+class ADCollapseBox extends HTMLElement implements ICollapseBox{
+    
+    get adId(){
         return this.getAttribute('ad-id');
     }
-    set adId(value) {
+
+    set adId(value){
         this.setAttribute('ad-id', value);
     }
-    get componentTitle() {
+
+    get componentTitle(){
         return this.getAttribute('ad-title');
     }
-    set componentTitle(value) {
+
+    set componentTitle(value){
         this.setAttribute('ad-title', value);
     }
+
     static get observedAttributes() {
         return ['ad-id', 'ad-title'];
     }
-    constructor() {
+
+    constructor(){
         super();
+        
         const content = this.innerHTML;
-        this.innerHTML = this.render({ content: content });
+        this.innerHTML = this.render({ content:content});
+        
     }
-    render(values) {
+
+    render(values:IContent): string {
         return `<ad-collapse-box-title>
             <ad-cb-title></ad-cb-title>
             <ad-cb-button></ad-cb-button-title>
         </ad-collapse-box-title>
         <ad-cb-content>${values.content}</ad-cb-content>`;
     }
-    addButton(html, action) {
+
+    addButton(html: string, action: (event: Event) => void): void {
+        
         throw new Error("Method not implemented.");
     }
-    highlightTitle() {
+
+    highlightTitle(): void {
         throw new Error("Method not implemented.");
     }
-    unHighlightTitle() {
+
+    unHighlightTitle(): void {
         throw new Error("Method not implemented.");
     }
-    close() {
+
+    close(): void {
         throw new Error("Method not implemented.");
     }
-    open() {
+
+    open(): void {
         throw new Error("Method not implemented.");
     }
 }
--- form/ts/form-elements.ts
+++ form/ts/form-elements.ts
@@ -1,3 +1,14 @@
+interface IInputGroup {
+    label: string;
+    type: string; 
+    subType?: string;
+}
+
+interface IParameter {
+    id: string;
+    type: string;
+}
+
 /**
  * ADInputGroup
  * ad-type
@@ -9,62 +20,73 @@
  *  multiselect
  *  checkbox
  *  radiobutton
- *
+ * 
  */
-class ADInputGroup extends HTMLElement {
-    constructor() {
+class ADInputGroup extends HTMLElement implements IInputGroup{
+    constructor(){
         super();
+
         const type = this.type;
         const subType = this.subType;
         const id = this.id;
         const label = this.label;
-        const typeHandlers = {
-            'text': (p) => {
+
+        const typeHandlers: {[index: string]:any} = {
+            'text': (p:IParameter) => { 
                 return `<input type="text" class="ad-form-control" name="${p.id}" ad-id="${p.id}">`;
             },
-            'search': (p) => {
+            'search': (p:IParameter) => {
                 return `
                     <input type="text" class="ad-form-control" name="${p.id}" ad-id="${p.id}">
                     <ad-icon class="ad-icon-button"><svg viewBox="0 0 24 24"><g><path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"></path></g></svg></ad-icon>`;
             },
-            'multiselect': (p) => {
+            'multiselect': (p:IParameter) => {
                 let subType = '';
-                if (p.type) {
-                    subType = `ad-type="${p.type}" `;
+                if(p.type){
+                    subType = `ad-type="${p.type}" `
                 }
                 return `<div ad-id="${p.id}" ${subType}ad-ms-default-text="Please Select"></div>`;
             },
-            'date': (p) => {
+            'date':  (p:IParameter) => {
                 return `<input type="date" class="ad-form-control" name="${p.id}" ad-id="${p.id}">`;
             },
-            'datetime': (p) => {
+            'datetime': (p:IParameter) => {
                 return `<input type="datetime-local" class="ad-form-control" name="${p.id}" ad-id="${p.id}">`;
             }
-        };
+        }
         this.id = '';
+
         this.innerHTML = `
           <label for="${id}" class="ad-input-group-text">${label}</label>
-          <ad-control-group class="ad-control-group">${typeHandlers[type]({ id: id, subType: subType })}</ad-control-group>`;
+          <ad-control-group class="ad-control-group">${typeHandlers[type]({id: id, subType: subType})}</ad-control-group>`;
     }
+
     static get observedAttributes() {
         return ['ad-type', 'ad-label'];
     }
-    get type() {
+
+    get type(){
         return this.getAttribute('ad-type') || '';
     }
-    set type(value) {
+
+    set type(value:string){
         this.setAttribute('ad-type', value);
     }
-    get subType() {
+
+    get subType(){
         return this.getAttribute('ad-sub-type') || '';
     }
-    set subType(value) {
+
+    set subType(value:string){
         this.setAttribute('ad-sub-type', value);
     }
-    get label() {
+
+
+    get label(){
         return this.getAttribute('ad-title') || '';
     }
-    set label(value) {
+
+    set label(value: string){
         this.setAttribute('ad-title', value);
     }
 }
